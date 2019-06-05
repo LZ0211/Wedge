d59cc9a79918a6b79b050f7b4348179561a640ba
Liezhang0211 <1491097321@qq.com>
2019-06-06 03:37:58 +0800

    update
    add rules

---
+++ config/Sites/default/www.ireader.com/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "host":"www.ireader.com",
+    "match":[
+        "www.ireader.com"
+    ],
+    "charset":"utf8",
+    "selector":require("./selector"),
+    "replacer":require("./replacer")
+}
